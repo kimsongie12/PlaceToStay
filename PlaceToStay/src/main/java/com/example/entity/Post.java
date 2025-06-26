25fe5955a1999226817d43a438e345d677eca5db
kimsongie12 <song3078@naver.com>
2025-06-26 16:02:37 +0900

    settings

--- PlaceToStay/src/main/java/com/example/entity/Post.java
+++ PlaceToStay/src/main/java/com/example/entity/Post.java
@@ -1,5 +1,41 @@
 package com.example.entity;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Post")
+@Data
+@NoArgsConstructor
 public class Post {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer postId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private String title;
+
+    @Lob
+    private String content;
+
+    private Integer viewCnt = 0;
+
+    private Boolean isSolved = false;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
 }
