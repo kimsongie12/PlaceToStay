f8e1e2b9e6cb4be309daabdfc5a7188c38f55905
kimsongie12 <song3078@naver.com>
2025-06-26 16:05:02 +0900

    feat: PostService

---
+++ PlaceToStay/src/main/java/com/example/service/PostService.java
@@ -0,0 +1,33 @@
+package com.example.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Post;
+import com.example.repository.PostRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+
+    private final PostRepository postRepository;
+
+    public List<Post> getAllPosts() {
+        return postRepository.findAllByOrderByCreatedAtDesc();
+    }
+
+    public Post getPostById(Integer id) {
+        return postRepository.findById(id).orElseThrow();
+    }
+
+    public void savePost(Post post) {
+        postRepository.save(post);
+    }
+
+    public void deletePost(Integer id) {
+        postRepository.deleteById(id);
+    }
+}
