624ea2fab7619ad4d50efb61c20590a94054dfcb
kimsongie12 <song3078@naver.com>
2025-06-26 14:42:31 +0900

    settings

---
+++ PlaceToStay/src/main/java/com/example/test/PlaceToStayApplication.java
@@ -0,0 +1,13 @@
+package com.example.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaceToStayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaceToStayApplication.class, args);
+	}
+
+}
